02ec4f2272be5b12a2f993d7f651b063d4237f27
Mak <mak@dynamic-biosensors.com>
2023-08-31 14:05:36 +0200

    refactor: Add test fixture for creation of new user

--- test_tripservice.py
+++ test_tripservice.py
@@ -8,11 +8,15 @@ def find_trips_by_user(user):
     return user.trips
 
 
+@pytest.fixture
+def new_user():
+    return User()
+
+
 class TestWithoutLoggedUser:
 
     @patch('tripservice._getLoggedUser', return_value="")
-    def test_get_trips_raises_exception(self, patch_getLoggedUser):
-        new_user = User()
+    def test_get_trips_raises_exception(self, patch_getLoggedUser, new_user):
         with pytest.raises(UserNotLoggedInException):
             getTripsByUser(new_user)
 
@@ -21,17 +25,14 @@ class TestWithoutLoggedUser:
 @patch('tripservice._findTripsByUser', wraps=find_trips_by_user)
 class TestWithLoggedUser:
 
-    def test_get_trips_returns_empty_if_user_not_friend(self, patch_getLoggedUser, patch_findTripsByUser):
-        new_user = User()
+    def test_get_trips_returns_empty_if_user_not_friend(self, patch_getLoggedUser, patch_findTripsByUser, new_user):
         assert getTripsByUser(new_user) == []
 
-    def test_get_trips_returns_empty_for_friends_without_trips(self, patch_getLoggedUser, patch_findTripsByUser):
-        new_user = User()
+    def test_get_trips_returns_empty_for_friends_without_trips(self, patch_getLoggedUser, patch_findTripsByUser, new_user):
         new_user.addFriend("Tom")
         assert getTripsByUser(new_user) == []
 
-    def test_get_trips_returns_trips_for_friends(self, patch_getLoggedUser, patch_findTripsByUser):
-        new_user = User()
+    def test_get_trips_returns_trips_for_friends(self, patch_getLoggedUser, patch_findTripsByUser, new_user):
         new_user.addFriend("Tom")
         new_user.addTrip("trip_1")
         assert "trip_1" in getTripsByUser(new_user)
