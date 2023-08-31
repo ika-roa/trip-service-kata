b35892fb09a510c07c201b91dddbdf8741df7381
Mak <mak@dynamic-biosensors.com>
2023-08-31 13:29:21 +0200

    test: Ensure getTrips raises exception if no user logged in

--- test_tripservice.py
+++ test_tripservice.py
@@ -1,1 +1,7 @@
+import pytest
+from tripservice import User, getTripsByUser, _getLoggedUser
 
+
+def test_get_trips_from_new_user_returns_empty():
+    new_user = User()
+    getTripsByUser(new_user)
