164920d2b45843067360a992ddd661001ba114c3
Mak <mak@dynamic-biosensors.com>
2023-08-31 13:42:03 +0200

    test: Ensure getTrips returns empty list if user is not friend of logged user

--- test_tripservice.py
+++ test_tripservice.py
@@ -1,7 +1,24 @@
 import pytest
-from tripservice import User, getTripsByUser, _getLoggedUser
+from tripservice import User, getTripsByUser, UserNotLoggedInException
+
+from unittest.mock import patch
 
 
-def test_get_trips_from_new_user_returns_empty():
-    new_user = User()
-    getTripsByUser(new_user)
+class TestWithoutLoggedUser:
+
+    @patch('tripservice._getLoggedUser', return_value="")
+    def test_get_trips_raises_exception(self, patch_func):
+        new_user = User()
+        with pytest.raises(UserNotLoggedInException):
+            getTripsByUser(new_user)
+
+
+class TestWithLoggedUser:
+
+    @patch('tripservice._getLoggedUser', return_value="Tom")
+    def test_get_trips_returns_empty_if_user_not_friend(self, patch_func):
+        new_user = User()
+        assert getTripsByUser(new_user) == []
+
+
+
