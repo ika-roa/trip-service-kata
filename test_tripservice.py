c706389dca1d0f8dcb2cab10829b71d3d4026afa
Mak <mak@dynamic-biosensors.com>
2023-08-31 13:54:59 +0200

    test: Ensure getTrips returns empty list if user is friend of logged user and has no trips

--- test_tripservice.py
+++ test_tripservice.py
@@ -4,10 +4,14 @@ from tripservice import User, getTripsByUser, UserNotLoggedInException
 from unittest.mock import patch
 
 
+def find_trips_by_user(user):
+    return user.trips
+
+
 class TestWithoutLoggedUser:
 
     @patch('tripservice._getLoggedUser', return_value="")
-    def test_get_trips_raises_exception(self, patch_func):
+    def test_get_trips_raises_exception(self, patch_getLoggedUser):
         new_user = User()
         with pytest.raises(UserNotLoggedInException):
             getTripsByUser(new_user)
@@ -16,8 +20,15 @@ class TestWithoutLoggedUser:
 class TestWithLoggedUser:
 
     @patch('tripservice._getLoggedUser', return_value="Tom")
-    def test_get_trips_returns_empty_if_user_not_friend(self, patch_func):
+    def test_get_trips_returns_empty_if_user_not_friend(self, patch_getLoggedUser):
         new_user = User()
+        assert getTripsByUser(new_user) == []
+
+    @patch('tripservice._getLoggedUser', return_value="Tom")
+    @patch('tripservice._findTripsByUser', wraps=find_trips_by_user)
+    def test_get_trips_returns_empty_for_freinds_without_trips(self, patch_getLoggedUser, patch_findTripsByUser):
+        new_user = User()
+        new_user.addFriend("Tom")
         assert getTripsByUser(new_user) == []
 
 
